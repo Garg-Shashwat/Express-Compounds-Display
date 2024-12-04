ab1bea3a269c7c8022f1d0a2de2b4a5c0008e775
Garg-Shashwat <gargshashwat007@gmail.com>
2024-12-04 17:00:04 +0530

    chore; add data dump

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import db from './models/index.js';
 import 'dotenv/config';
 import { setupServer, startServer } from './transport/server.js'
+import { dumpData } from './migrations/data_dump/default_data_dump.js';
 
 // DB setup
 (async () => {
@@ -9,6 +10,11 @@ import { setupServer, startServer } from './transport/server.js'
 
     await db.conn.sync({ force: false });
     console.log('Database synced successfully.');
+
+    if (process.env.DUMP_DATA == 'true') {
+        console.log('Dumping Data');
+        dumpData('/usr/src/app/data_dump.csv');
+    }
 })();
 
 const app = express();
---
+++ src/migrations/data_dump/default_data_dump.js
@@ -0,0 +1,48 @@
+import mysql from 'mysql2';
+import fs from 'fs';
+import csv from 'csv-parser';
+
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+};
+
+export const dumpData = async (filePath) => {
+    const connection = mysql.createConnection(dbConfig);
+    const records = [];
+
+    fs.createReadStream(filePath)
+        .pipe(csv())
+        .on('data', (row) => {
+            records.push(row);
+        })
+        .on('end', () => {
+            console.log('CSV file successfully processed.');
+            console.log(records[0]);
+
+            const insertQuery = `
+                INSERT INTO Compounds (name, description, image, imageAttribution, createdAt, updatedAt) 
+                VALUES ?`;
+
+            const values = records.map(record => [
+                record.name, 
+                record.description, 
+                record.image, 
+                record.imageAttribution,
+                new Date(),
+                new Date(),
+            ]);
+
+            connection.query(insertQuery, [values], (err, result) => {
+                if (err) {
+                    console.error('Error inserting data:', err);
+                    return;
+                }
+                console.log('Data dumped successfully:', result.affectedRows);
+            });
+
+            connection.end();
+        });
+};
